fdc43546aa2b51de70ddcc79d4e0a018db16dd02
Chirag <153700720+imchirag29@users.noreply.github.com>
2025-08-11 03:15:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('openMenu').addEventListener('click', function(){
+  document.getElementById('menu').style.display = 'block';
+});
+document.getElementById('closeMenu').addEventListener('click', function(){
+  document.getElementById('menu').style.display = 'none';
+});
